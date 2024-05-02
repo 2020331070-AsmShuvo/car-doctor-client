b3affff130744cf0490bd63f991d925fc99e7f21
Asm Shuvo <shuvo.sust.cse@gmail.com>
2024-05-03 02:56:19 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,319 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import img1 from "../../../assets/images/banner/1.jpg";
+import img2 from "../../../assets/images/banner/2.jpg";
+import img3 from "../../../assets/images/banner/3.jpg";
+import img4 from "../../../assets/images/banner/4.jpg";
+import img5 from "../../../assets/images/banner/5.jpg";
+import img6 from "../../../assets/images/banner/6.jpg";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="carousel w-full h-[600px]">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img1} className="w-full" />
+          <div className="absolute h-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]  flex flex-col left-0 top-0 gap-4">
+            <div className="space-y-7 pl-16 mt-16">
+              <h1 className="text-6xl w-1/4 text-white font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p className="text-lg w-1/2 text-gray-200">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex flex-col md:flex-row gap-2">
+                <Link
+                  to="/discover"
+                  class="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                >
+                  <span class="absolute -start-full transition-all group-hover:start-4">
+                    <svg
+                      class="size-5 rtl:rotate-180"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </span>
+
+                  <span class="text-sm font-medium transition-all group-hover:ms-4">
+                    Discover More
+                  </span>
+                </Link>
+                <Link
+                  class="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                  to="/latest"
+                >
+                  <span class="absolute -end-full transition-all group-hover:end-4">
+                    <svg
+                      class="size-5 rtl:rotate-180"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </span>
+
+                  <span class="text-sm font-medium transition-all group-hover:me-4">
+                    {" "}
+                    Download{" "}
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img2} className="w-full" />
+          <div className="absolute h-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]  flex flex-col left-0 top-0 gap-4">
+            <div className="space-y-7 pl-16 mt-16">
+              <h1 className="text-6xl w-1/4 text-white font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p className="text-lg w-1/2 text-gray-200">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex flex-col md:flex-row gap-2">
+                <Link
+                  to="/discover"
+                  class="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                >
+                  <span class="absolute -start-full transition-all group-hover:start-4">
+                    <svg
+                      class="size-5 rtl:rotate-180"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </span>
+
+                  <span class="text-sm font-medium transition-all group-hover:ms-4">
+                    Discover More
+                  </span>
+                </Link>
+                <Link
+                  class="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                  to="/latest"
+                >
+                  <span class="absolute -end-full transition-all group-hover:end-4">
+                    <svg
+                      class="size-5 rtl:rotate-180"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </span>
+
+                  <span class="text-sm font-medium transition-all group-hover:me-4">
+                    {" "}
+                    Download{" "}
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={img3} className="w-full" />
+          <div className="absolute h-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]  flex flex-col left-0 top-0 gap-4">
+            <div className="space-y-7 pl-16 mt-16">
+              <h1 className="text-6xl w-1/4 text-white font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p className="text-lg w-1/2 text-gray-200">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex flex-col md:flex-row gap-2">
+                <Link
+                  to="/discover"
+                  class="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                >
+                  <span class="absolute -start-full transition-all group-hover:start-4">
+                    <svg
+                      class="size-5 rtl:rotate-180"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </span>
+
+                  <span class="text-sm font-medium transition-all group-hover:ms-4">
+                    Discover More
+                  </span>
+                </Link>
+                <Link
+                  class="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                  to="/latest"
+                >
+                  <span class="absolute -end-full transition-all group-hover:end-4">
+                    <svg
+                      class="size-5 rtl:rotate-180"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </span>
+
+                  <span class="text-sm font-medium transition-all group-hover:me-4">
+                    {" "}
+                    Download{" "}
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={img4} className="w-full" />
+          <div className="absolute h-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]  flex flex-col left-0 top-0 gap-4">
+            <div className="space-y-7 pl-16 mt-16">
+              <h1 className="text-6xl w-1/4 text-white font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p className="text-lg w-1/2 text-gray-200">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex flex-col md:flex-row gap-2">
+                <Link
+                  to="/discover"
+                  class="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                >
+                  <span class="absolute -start-full transition-all group-hover:start-4">
+                    <svg
+                      class="size-5 rtl:rotate-180"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </span>
+
+                  <span class="text-sm font-medium transition-all group-hover:ms-4">
+                    Discover More
+                  </span>
+                </Link>
+                <Link
+                  class="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                  to="/latest"
+                >
+                  <span class="absolute -end-full transition-all group-hover:end-4">
+                    <svg
+                      class="size-5 rtl:rotate-180"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  </span>
+
+                  <span class="text-sm font-medium transition-all group-hover:me-4">
+                    {" "}
+                    Download{" "}
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/services">Services</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+            </ul>
+          </div>
+          <Link to="/">
+            <img src={logo} className="w-16 ml-4" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/services">Services</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <Link
+            to="/appoint"
+            class="group relative inline-block overflow-hidden border border-[#6a50b0] px-8 py-1 focus:outline-none focus:ring"
+          >
+            <span class="absolute inset-y-0 left-0 w-[2px] bg-[#6a50b0] transition-all group-hover:w-full group-active:bg-indigo-500"></span>
+
+            <span class="relative text-sm font-medium text-[#6a50b0] transition-colors group-hover:text-white">
+              Appointment
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
